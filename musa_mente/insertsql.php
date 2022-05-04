0a30bbfa3c532af812f2621ce0cd6ffdde697802
William Camargo <44587916+hotwyl@users.noreply.github.com>
2022-05-04 20:07:44 -0300

    ajuste conexão musa da mente

--- musa_mente/insertsql.php
+++ musa_mente/insertsql.php
@@ -5,13 +5,23 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-$servername="mysql743.umbler.com:41890";
+// $servername="mysql743.umbler.com:41890";
 
-$username="adima";
+// $username="adima";
 
-$password="3278Data";
+// $password="3278Data";
 
-$dbname="dataarqmm";
+// $dbname="dataarqmm";
+
+$servername="nspro32.hostgator.com.br:3306";
+
+$username="hg3won41_hotwyl";
+
+$password="willfrombrasil";
+
+$dbname="hg3won41_musa_mente";
+
+
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
